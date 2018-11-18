0a189b78b7ac0660a3cde1fed982848b607990d0
Ironbody <simon.saberian@gmail.com>
2018-11-18 16:20:59 +0100

    Implemented mentioning user and replying with a sentence

--- index.js
+++ index.js
@@ -5,34 +5,19 @@ const config = JSON.parse(fs.readFileSync("./config.json"));
 
 const discordClient = new Discord.Client();
 
-let isChecking = false;
-let interval;
+function getRandomSentence() {
+  let random = Math.floor(Math.random() * config.sentences.length);
+  return config.sentences[random];
+}
 
 discordClient.once("ready", () => {
   console.log("Up and running!");
 });
 
 discordClient.on("message", async message => {
-  if (message.content == "!start" && !isChecking) {
-    if (!message.member.roles.some(r => ["admin"].includes(r.name))) {
-      return message.channel.send("Nope");
-    } else {
-      message.channel.send("Starting check");
-      isChecking = true;
-
-      interval = setInterval(function() {
-        message.channel.send("Check");
-      }, 1000);
-    }
-  } else if (message.content == "!stop" && isChecking) {
-    if (!message.member.roles.some(r => ["admin"].includes(r.name))) {
-      return message.channel.send("Nope");
-    } else {
-      message.channel.send("Stopping check");
-      isChecking = false;
-
-      clearInterval(interval);
-    }
+  if (message.content == config.command) {
+    let sentence = getSentence();
+    message.channel.send(`@${message.author} ${sentence}`);
   }
 });
 
